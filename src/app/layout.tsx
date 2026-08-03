f4a42f925ae899cf34baab1f1dbad787e60ad29c
Anonymous Analyst <analyst@example.com>
2026-08-03 13:08:53 +0530

    fix(ui): load material symbols font to fix missing icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,7 @@ export default function RootLayout({
     >
       <head>
         <meta name="darkreader-lock" />
+        <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" rel="stylesheet" />
       </head>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
